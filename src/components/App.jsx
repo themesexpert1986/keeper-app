5f6f84752daf5929d27925724651459b588620de
Vinh Nguyen <themesexperts1986@gmail.com>
2020-05-02 23:24:12 +0700

    Inital Code

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App(){
+    return <div className="container"><Header /> <Note /> <Footer /> </div>
+}
+
+export default App;
